34f8622a85b96a0c913b9721b650492b976db667
dnguyen19 <dnguyen145@my.bcit.ca>
2019-01-30 13:02:55 -0800

    updated

--- host_network_setup.sh
+++ host_network_setup.sh
@@ -1,18 +1,21 @@
 vboxmanage () { VBoxManage.exe "$@";}
 
-vboxmanage natnetwork add 
-	--netname "sys_net_prov" 
-	--network "192.168.254.0/24" 
-	--dhcp off
+declare vm_name="acit4640"
+declare vm_folder="C:\Users\denis\Documents\CIT\CIT4\provision"
+
+declare network_name="sys_net_prov"
+declare network_address="*"
+declare cidr_bits="24"
+declare local_ip="192.168.254.10"
+ 
+declare port_num=0
+declare devic_num=0
+declare ios_file_path="C:\Users\denis\Documents\CIT\CIT4\provision\CentOS-7-x86_64-Minimal-1810.iso"
+
+vboxmanage natnetwork add --netname ${network_name} --network "${network_address}/${cidr_bits}" --dhcp off
+
+vboxmanage natnetwork modify --netname ${network_name} --port-forward-4 "rule_1:TCP:[${network_address}]:50022:[${local_ip}]:22"
 	
-vboxmanage natnetwork modify 
-	--netname "sys_net_prov"
-	--port-forward-4 "rule_1:TCP:[192.168.254.10]:50022:[192.168.254.10]:22"
+vboxmanage natnetwork modify --netname ${network_name} --port-forward-4 "rule_2:TCP:[${network_address}]:50080:[${local_ip}]:80"
 	
-vboxmanage natnetwork modify 
-	--netname "sys_net_prov"
-	--port-forward-4 "rule_2:TCP:[192.168.254.10]:50080:[192.168.254.10]:80"
-	
-vboxmanage natnetwork modify 
-	--netname "sys_net_prov"
-	--port-forward-4 "rule_3:TCP:[192.168.254.10]:50443:[192.168.254.10]:443"
+vboxmanage natnetwork modify --netname ${network_name} --port-forward-4 "rule_3:TCP:[${network_address}]:50443:[${local_ip}]:443"
--- wp_vm_setup.sh
+++ wp_vm_setup.sh
@@ -1,10 +1,45 @@
 vboxmanage () { VBoxManage.exe "$@";}
 
+declare ctrlr_type_1='ide'
+declare ctrlr_type_2='sata'
+declare vm_name="acit4640"
+declare port_num=0
+declare devic_num=0
+declare network_name="sys_net_prov"
+declare vm_folder="C:\Users\denis\Documents\CIT\CIT4\provision"
+declare ios_file_path="C:\Users\denis\Documents\CIT\CIT4\provision\CentOS-7-x86_64-Minimal-1810.iso"
 
-vboxmanage createvm --name WP_VM --register
+vboxmanage createvm --name ${vm_name} --register
 
-vboxmanage modifyvm WP_VM\
-            --groups "${group_name}"\
+vboxmanage createhd --filename ${vm_folder}//${vm_name}.vdi --size 8096 -variant Standard
+					
+vboxmanage storagectl ${vm_name} --name 'ide_controller' --add ${ctrlr_type_1} --bootable on
+vboxmanage storagectl ${vm_name} --name 'sata_controller' --add ${ctrlr_type_2} --bootable on
+										
+vboxmanage storageattach ${vm_name} \
+					--storagectl 'ide_controller' \
+					--port ${port_num} \
+					--device ${devic_num} \
+					--type dvddrive \
+					--medium ${ios_file_path}
+					
+vboxmanage storageattach ${vm_name} \
+					--storagectl 'ide_controller' \
+					--port ${port_num} \
+					--device ${devic_num} \
+					--type dvddrive \
+					--medium ${vm_folder}\\${vm_name}.vdi
+					
+vboxmanage storageattach ${vm_name} \
+					--storagectl 'sata_controller' \
+					--port $port_num \
+					--device $devic_num \
+					--type hdd \
+					--medium ${vm_folder}//${vm_name}.vdi \
+					--nonrotational on
+					
+					
+vboxmanage modifyvm ${vm_name}\
             --ostype "RedHat_64"\
             --cpus 1\
             --hwvirtex on\
@@ -12,13 +47,13 @@ vboxmanage modifyvm WP_VM\
             --largepages on\
             --firmware bios\
             --nic1 natnetwork\
-            --nat-network1 "sys_net_prov"\
+            --nat-network1 "${network_name}"\
             --cableconnected1 on\
             --audio none\
             --boot1 disk\
             --boot2 dvd\
             --boot3 none\
             --boot4 none\
-            --memory "1280"
+            --memory 1280
 			
-vboxmanage startvm WP_VM --type gui
+#vboxmanage startvm ${vm_name} --type gui
