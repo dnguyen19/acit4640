a70020fb11be3749856542a9143973a6223b4b61
dnguyen19 <dnguyen145@my.bcit.ca>
2019-03-05 20:07:03 -0800

    Kickstart

--- scripted/wp_vm_setup.sh
+++ scripted/wp_vm_setup.sh
@@ -2,11 +2,12 @@ vboxmanage () { VBoxManage.exe "$@";}
 
 declare ctrlr_type_1='ide'
 declare ctrlr_type_2='sata'
-declare vm_name="acit4640"
+declare vm_name="acit_4640"
 declare port_num=0
 declare devic_num=0
 declare network_name="sys_net_prov"
 declare vm_folder="C:\Users\denis\Documents\CIT\CIT4\provision"
+declare script_dir="C:\Users\denis\Documents\CIT\CIT4\provision\acit4640"
 declare ios_file_path="C:\Users\denis\Documents\CIT\CIT4\provision\CentOS-7-x86_64-Minimal-1810.iso"
 
 vboxmanage createvm --name ${vm_name} --register
@@ -51,9 +52,29 @@ vboxmanage modifyvm ${vm_name}\
             --cableconnected1 on\
             --audio none\
             --boot1 disk\
-            --boot2 dvd\
+            --boot2 net\
             --boot3 none\
             --boot4 none\
+			      --macaddress1 020000000001\
             --memory 1280
-			
+
+
 vboxmanage startvm ${vm_name} --type gui
+
+sudo chown -R admin:nginx /usr/share/nginx/html*
+
+scp ${script_dir}/kickstart/wp_ks.cfg pxe:/usr/share/nginx/html/
+scp -r ${script_dir}/setup pxe:/usr/share/nginx/html/
+ssh pxe 'sudo chown nginx:wheel /usr/share/nginx/html/wp_ks.cfg'
+ssh pxe 'chmod ugo+r /usr/share/nginx/html/wp_ks.cfg'
+ssh pxe 'chmod ugo+rx /usr/share/nginx/html/setup'
+ssh pxe 'chmod -R ugo+r /usr/share/nginx/html/setup/*'
+
+until [[ $(ssh -q wp exit && echo "online") == "online" ]] ; do
+  sleep 10s
+  echo "waiting for wp vm to come online"
+done
+
+##ssh pxe
+##bash wp_vm_setup.sh
+##localhost 50080
